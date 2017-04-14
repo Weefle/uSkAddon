75ad22e57a73266324f3a70d66e95320e36f61bb
Leonardo <Leonardo@DESKTOP-KMC5QQH>
2017-04-14 13:50:04 +0200

    Reordered code

--- src/rublitio/uskaddon/utils/EnumRegister.java
+++ src/rublitio/uskaddon/utils/EnumRegister.java
@@ -1,11 +1,9 @@
 package rublitio.uskaddon.utils;
 
 public class EnumRegister {
-
-	
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <E> void registerEnum(Class cls, String codeName){
-		EnumClassInfo.create(cls, codeName).register();;
+		EnumClassInfo.create(cls, codeName).register();
 	}
 }
